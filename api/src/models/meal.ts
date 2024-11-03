6cee3c1fbc27b33b95ca697c1bf233340252dc1c
Giovanni Olmos <creatuintro123@gmail.com>
2024-11-03 01:50:57 -0300

    cambio en model meal y creacion de model user

--- api/src/models/meal.ts
+++ api/src/models/meal.ts
@@ -1,11 +1,11 @@
 import { model, Schema } from "mongoose";
 import { IMeal } from "../interfaces/meals";
 
-const mealSchema = new Schema<IMeal>({
+const mealSchema = new Schema({
   name: { type: String, required: true, minlength: 2, maxlength: 50 },
   description: { type: String, required: true, minlength: 10 },
   imageUrl: { type: String, required: true },
   category: { type: String, required: true },
 });
 
-export const Meal = model<IMeal>("Meal", mealSchema);
+export const Meal = model("Meal", mealSchema);
---
+++ api/src/models/user.ts
@@ -0,0 +1,10 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true, trim: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
+});
+
+export const Users = model("Users", userSchema);

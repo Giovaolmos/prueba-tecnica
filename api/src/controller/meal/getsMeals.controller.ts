892dc81e924381ae853b53132d84f7f3b9b022d3
Giovanni Olmos <creatuintro123@gmail.com>
2024-11-03 04:34:50 -0300

    paginación en getallmeals

--- api/src/controller/meal/getsMeals.controller.ts
+++ api/src/controller/meal/getsMeals.controller.ts
@@ -5,12 +5,19 @@ import {
 } from "../../services/meal/getsMeal.service";
 
 export const getAllMealsController = async (req: Request, res: Response) => {
-  const mealsDB = await getMealsService();
+  const page = parseInt(req.query.page as string) || 1;
+  const limit = parseInt(req.query.limit as string) || 10;
+
+  const mealsDB = await getMealsService(page, limit);
 
   if (mealsDB.length === 0) {
     res.status(204).json({ message: "No se encontraron comidas" });
   } else {
-    res.status(200).json(mealsDB);
+    res.status(200).json({
+      data: mealsDB,
+      page,
+      limit,
+    });
   }
 };
 export const getMealByIdController = async (req: Request, res: Response) => {
--- api/src/services/meal/getsMeal.service.ts
+++ api/src/services/meal/getsMeal.service.ts
@@ -1,8 +1,12 @@
 import { IMeal } from "../../interfaces/meals";
 import { Meal } from "../../models/meal";
 
-export const getMealsService = async (): Promise<IMeal[]> => {
-  return await Meal.find();
+export const getMealsService = async (
+  page: number,
+  limit: number,
+): Promise<IMeal[]> => {
+  const skip = (page - 1) * limit;
+  return await Meal.find().skip(skip).limit(limit);
 };
 export const getMealByIdService = async (id: string): Promise<IMeal | null> => {
   const meal = await Meal.findById(id);

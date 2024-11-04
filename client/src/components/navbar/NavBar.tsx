ceb5f8e8c8826b56e87f50e99f83b7aa368b6421
Giovanni Olmos <creatuintro123@gmail.com>
2024-11-04 20:02:48 -0300

    navbar

--- client/src/components/navbar/NavBar.tsx
+++ client/src/components/navbar/NavBar.tsx
@@ -6,20 +6,13 @@ export const NavBar: React.FC = () => {
   return (
     <div>
       <header>
-        <nav className="flex items-center justify-between p-4 bg-transparent text-white relative z-10 bg-gray-700">
+        <nav className="flex items-center justify-between p-4 bg-transparent text-white relative z-10 bg-slate-700">
           <h1>The Meal DB</h1>
           <div className="hidden lg:flex ml-auto">
             <ul className="flex gap-4 items-center cursor-pointer">
               <li>Inicio</li>
-              <li>Historial</li>
-              <li>Nuevo Turno</li>
-              <li>Home</li>
-              <li className="rounded-md bg-[#b37d44] py-1 px-2 hover:bg-[#dc9c57]">
-                Iniciar Sesión
-              </li>
-              <li className="rounded-md bg-[#de8730] py-1 px-2 hover:bg-[#f29435]">
-                Registrar
-              </li>
+              <li>Iniciar Sesión</li>
+              <li>Registrar</li>
             </ul>
           </div>
           <button
@@ -32,7 +25,7 @@ export const NavBar: React.FC = () => {
       </header>
 
       {isMenuOpen && (
-        <div className="lg:hidden bg-gray-700 text-white p-4">
+        <div className="lg:hidden bg-slate-700 text-white p-4">
           <ul className="flex flex-col gap-2">
             <li>Inicio</li>
             <li>Iniciar Sesión</li>

f872e7c1a75248248e783b65185624ab0d93f1aa
Giovanni Olmos <creatuintro123@gmail.com>
2024-11-04 21:23:47 -0300

    login form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import { NavBar } from "./components/navbar/NavBar";
+import { Login } from "./views/Login/Login";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
--- client/src/components/navbar/NavBar.tsx
+++ client/src/components/navbar/NavBar.tsx
@@ -6,13 +6,13 @@ export const NavBar: React.FC = () => {
   return (
     <div>
       <header>
-        <nav className="flex items-center justify-between p-4 bg-transparent text-white relative z-10 bg-slate-700">
+        <nav className="flex items-center justify-between p-4  text-white relative z-10 bg-slate-800">
           <h1>The Meal DB</h1>
           <div className="hidden lg:flex ml-auto">
             <ul className="flex gap-4 items-center cursor-pointer">
-              <li>Inicio</li>
-              <li>Iniciar Sesión</li>
-              <li>Registrar</li>
+              <li className="text-white  hover:text-slate-300">Home</li>
+              <li className="text-white  hover:text-slate-300">SignIn</li>
+              <li className="text-white  hover:text-slate-300">SignUp</li>
             </ul>
           </div>
           <button
@@ -25,7 +25,7 @@ export const NavBar: React.FC = () => {
       </header>
 
       {isMenuOpen && (
-        <div className="lg:hidden bg-slate-700 text-white p-4">
+        <div className="lg:hidden bg-slate-800 text-white p-4">
           <ul className="flex flex-col gap-2">
             <li>Inicio</li>
             <li>Iniciar Sesión</li>
